0edd8b7deab8b951db8c26c800a7d6a8a0049b83
Akos Fabian <fabian.akos@gmail.com>
2015-01-10 10:51:32 +0100

    using arguments

--- src/main/groovy/com/github/afabian80/vocabulary-manager/Main.groovy
+++ src/main/groovy/com/github/afabian80/vocabulary-manager/Main.groovy
@@ -2,6 +2,11 @@ package com.github.afabian80.vocabulary_manager
 
 class Main {
 	static void main(String[] args) {
-		println "Hello"
+		if(args.length != 2) {
+			println "Error: input files not defined"
+			println "Usage: ..."
+			System.exit(1)
+		}
+		args.each { println it }
 	}
 }
